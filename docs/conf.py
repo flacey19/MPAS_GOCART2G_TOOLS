eb08c2accca6a4157bcd0a18f15cf66902b657ac
flacey19 <38566026+flacey19@users.noreply.github.com>
2026-09-21 12:20:28 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,14 @@
+# docs/conf.py
 
+project = 'MPAS_GOCART2G_TOOLS'
+copyright = '2026, flacey19'
+author = 'flacey19'
+
+# Sphinx extensions (add theme or markdown support here if needed)
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# HTML theme configuration
+html_theme = 'sphinx_rtd_theme'
